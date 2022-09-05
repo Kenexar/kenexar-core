cf05e043dbdab430951a27a17480f7c48fe707c5
exersalza <squidodle44@gmail.com>
2022-09-05 13:09:00 +0200

    created setup.sh for the lazy ones

--- setup.sh
+++ setup.sh
@@ -1,21 +1,18 @@
-#
-# Copyright (c) 2022. exersalza
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
-# associated documentation files (the "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-# of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following
-# conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial
-# portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-# PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
-# OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-# OTHER DEALINGS IN THE SOFTWARE.
-#
-
 #!/bin/bash
+
+echo "Check for venv/"
+
+if [ -d "venv/" ]
+then
+  [[ "$VIRTUAL_ENV" ]] && source "venv/scripts/activate"
+else
+  eval python3 -m venv venv/
+  source "venv/scripts/activate"
+fi
+
+echo "Installing the Requirements"
+eval "pip3 install -r req.txt"
+
+echo "Run pylint to create a Difference between versions"
+# shellcheck disable=SC2046
+eval pylint $(git ls-files '*.py')
