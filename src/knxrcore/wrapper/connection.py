9530e63e822ab6051820d8dadb11ae4197847df1
exersalza <squidodle44@gmail.com>
2022-09-29 10:39:00 +0200

    Copyright update

--- src/knxrcore/wrapper/connection.py
+++ src/knxrcore/wrapper/connection.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2022. Lorem
+#  Copyright (c) 2022. exersalza
 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 #  associated documentation files (the "Software"), to deal in the Software without restriction, including
@@ -15,6 +15,7 @@
 #  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT
 #  OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 #  OTHER DEALINGS IN THE SOFTWARE.
+
 from http.client import HTTPSConnection
 
 
